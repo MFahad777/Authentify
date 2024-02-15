778d01d06e46e6a273b36f925079295c6b19552e
Muhammad Fahad <m.fahad1161681@gmail.com>
2024-02-15 18:18:41 +0500

    Facebook-auth | feat : Implemented facebook authentication

--- test.js
+++ test.js
@@ -4,25 +4,49 @@ const exp = require("express");
 
 const app = exp();
 
-const auth = new Authentication("AnewSecretToHaveInCommon", "jwt");
-
-const token = auth.generateToken({
-    userName:"johnDoe6978594",
-    firstName:"John",
-    lastName:"Doe",
-    email:"john_doe22@gmail.com"
-},{
-    expiresIn:60000,
+const auth = new Authentication({
+    jwtSecretKey:"AveryMuchSecretThatNoOneCanHack",
+    authMethod:"facebook",
+    facebookAppId:"<app_id>",
+    facebookAppSecret:"<secret_id>",
+    url:"http://localhost:3000",
+    callbackUrl:"",
+    facebookAPIVersion:"v19.0"
 });
 
-app.use(auth.authenticate())
+// Route for initiating the authentication process
+app.get('/auth/facebook', auth.facebookRedirect);
 
+app.get("/auth/facebook/callback", async (req,res,next) => {
 
-app.get("/",(req, res, next) => {
+    const token = await auth.generateToken({
+        jwt:{
+            options:{
+                expiresIn:6000
+            }
+        },
+        faceBook:{
+            code:req.query.code
+        }
+    })
 
-    console.log(req.auth)
+    res.redirect(`/dashboard?token=${token}`)
 
-    res.send(200);
+});
+
+app.get("/dashboard",(req, res, next) => {
+
+    res.send(`
+       <h1>${req.query.token}</h1>
+    `)
+
+})
+
+app.get("/protectedRoute",auth.authenticate(),(req, res, next) => {
+
+    res.json({
+        data: req.auth
+    });
 })
 
 app.listen(3000,() => console.log("LISTENING"))
